4feca7e8541951b665a65fbfd5070e15a4192823
PCu1106 <littlenenem@gmail.com>
2024-03-31 17:24:49 +0800

    UM_DSI_reversal corr analysis

--- data/UM_DSI_DB_v1.0.0_lite/code/tony_data_preprocessing_reverse.py
+++ data/UM_DSI_DB_v1.0.0_lite/code/tony_data_preprocessing_reverse.py
@@ -7,37 +7,13 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-
-# 讀取 2019-06-11 的 rssis.csv 文件
-with open('D:\\Experiment\\data\\UM_DSI_DB_v1.0.0_lite\\data\\site_surveys\\2020-02-19\\rssis.csv', 'r') as file:
-    lines = file.readlines()
-
-data0611 = []
-for line in lines:
-    ap_rssi_pairs = [pair.split(':') for pair in line.strip().split(',')]
-    row_data = {int(ap): int(rssi) for ap, rssi in ap_rssi_pairs}
-    data0611.append(row_data)
-
-# 將 2019-06-11 的資料轉換為 DataFrame
-df0611 = pd.DataFrame(data0611)
-
-# 讀取 2019-10-09 的 rssis.csv 文件
-with open('D:\\Experiment\\data\\UM_DSI_DB_v1.0.0_lite\\data\\site_surveys\\2019-10-09\\rssis.csv', 'r') as file:
-    lines = file.readlines()
-
-data1009 = []
-for line in lines:
-    ap_rssi_pairs = [pair.split(':') for pair in line.strip().split(',')]
-    row_data = {int(ap): int(rssi) for ap, rssi in ap_rssi_pairs}
-    data1009.append(row_data)
-
-# 將 2019-10-09 的資料轉換為 DataFrame
-df1009 = pd.DataFrame(data1009)
+from pathlib import Path
+import os
+import parser
 
 # 讀取 2020-02-19 的 rssis.csv 文件
-with open('D:\\Experiment\\data\\UM_DSI_DB_v1.0.0_lite\\data\\site_surveys\\2019-06-11\\rssis.csv', 'r') as file:
+with open('D:\\Experiment\\data\\UM_DSI_DB_v1.0.0_lite\\data\\site_surveys\\2020-02-19\\rssis.csv', 'r') as file:
     lines = file.readlines()
-
 data0219 = []
 for line in lines:
     ap_rssi_pairs = [pair.split(':') for pair in line.strip().split(',')]
@@ -46,93 +22,85 @@ for line in lines:
 
 # 將 2020-02-19 的資料轉換為 DataFrame
 df0219 = pd.DataFrame(data0219)
-print(df0219.shape)
-# 找出 df0611 中存在但 df1009 中不存在的 AP
-missing_aps1009 = [ap for ap in df0611.columns if ap not in df1009.columns]
-missing_aps0219 = [ap for ap in df0611.columns if ap not in df0219.columns]
-
-# 将缺失的 AP 添加到 df1009 中，并填充缺失值为 -100
-print(f'1009 miss {len(missing_aps1009)} aps')
-print(missing_aps1009)
-print(f'0219 miss {len(missing_aps0219)} aps')
-print(missing_aps0219)
-
-for ap in missing_aps1009:
-    df1009[ap] = -100
-
-for ap in missing_aps0219:
-    df0219[ap] = -100
-
-# 保留source domain資料
-print(df0611.shape)
-
-# 确保列的顺序和 df0611 一致
-df1009 = df1009[df0611.columns]
-df0219 = df0219[df0611.columns]
-# 使用插值處理缺失值
-df0611 = df0611.interpolate(method='linear', limit_direction='both')
-df1009 = df1009.interpolate(method='linear', limit_direction='both')
 df0219 = df0219.interpolate(method='linear', limit_direction='both')
-
-# 如果還有缺失值，則填補為-100
-df0611 = df0611.fillna(-100)
-df1009 = df1009.fillna(-100)
 df0219 = df0219.fillna(-100)
-
-# 處理 label0611.csv
-label_df0611 = pd.read_csv('labels200219.csv', header=0)
-df0611.insert(0, 'label', label_df0611['label'])
-
-# 處理 label1009.csv
-label_df1009 = pd.read_csv('labels191009.csv', header=0)
-df1009.insert(0, 'label', label_df1009['label'])
-
 # 處理 label0219.csv
-label_df0219 = pd.read_csv('labels190611.csv', header=0)
+label_df0219 = pd.read_csv('labels_2020-02-19.csv', header=0)
 df0219.insert(0, 'label', label_df0219['label'])
-
 # 計算每個AP的平均值和標準差
-mean_values = df0611.iloc[:, 1:].mean()
-std_values = df0611.iloc[:, 1:].std()
-
-# 對每個AP的RSSI做標準化
-for ap in mean_values.index:
-    if std_values[ap] == 0:
-        df0611[ap] = 0  # 或者使用其他值代替 0
-    else:
-        df0611[ap] = (df0611[ap] - mean_values[ap]) / std_values[ap]
-
-mean_values = df1009.iloc[:, 1:].mean()
-std_values = df1009.iloc[:, 1:].std()
-
-for ap in mean_values.index:
-    if std_values[ap] == 0:
-        df1009[ap] = 0  # 或者使用其他值代替 0
-    else:
-        df1009[ap] = (df1009[ap] - mean_values[ap]) / std_values[ap]
-
 mean_values = df0219.iloc[:, 1:].mean()
 std_values = df0219.iloc[:, 1:].std()
 
+
+# 對每個AP的RSSI做標準化
 for ap in mean_values.index:
     if std_values[ap] == 0:
         df0219[ap] = 0  # 或者使用其他值代替 0
     else:
         df0219[ap] = (df0219[ap] - mean_values[ap]) / std_values[ap]
 
-# 將處理後的資料存為 csv 檔案
-df0611.to_csv('data200219.csv', index=False)
-df1009.to_csv('data191009.csv', index=False)
-df0219.to_csv('data190611.csv', index=False)
+df0219.to_csv('data_2020-02-19.csv', index=False)
 
-for df, name in zip([df0611, df1009, df0219], ['200219', '191009', '190611']):
-    testing_set = pd.DataFrame(columns=df.columns)
-    for label in df['label'].unique():
-        sample = df[df['label'] == label].sample(n=1)
-        testing_set = pd.concat([testing_set, sample], ignore_index=True)
-        df = df.drop(sample.index)
-        print(f"  Label{label} samples: {len(df[df['label'] == label])}")
 
-    testing_set.to_csv(f'wireless_testing_{name}.csv', index=False)
-    df.to_csv(f'wireless_training_{name}.csv', index=False)
+dataset_folder = Path('..')
+ssv_folder = dataset_folder/'data'/'site_surveys'
+site_surveys = [f.name for f in os.scandir(ssv_folder) if f.is_dir()]
+site_surveys = parser.sorted_nicely(site_surveys)
+
+for site_survey in site_surveys:
+    if(site_survey == '2019-02-19' or site_survey == '2019-03-25' or site_survey == '2020-02-19' or site_survey == '2021-04-23'): # number of RP different, don't use them
+        continue
+    # 讀取 2019-10-09 的 rssis.csv 文件
+    with open(f'D:\\Experiment\\data\\UM_DSI_DB_v1.0.0_lite\\data\\site_surveys\\{site_survey}\\rssis.csv', 'r') as file:
+        lines = file.readlines()
+
+    data = []
+    for line in lines:
+        ap_rssi_pairs = [pair.split(':') for pair in line.strip().split(',')]
+        row_data = {int(ap): int(rssi) for ap, rssi in ap_rssi_pairs}
+        data.append(row_data)
+
+    # 將 2019-10-09 的資料轉換為 DataFrame
+    df = pd.DataFrame(data)
+    missing_aps = [ap for ap in df0219.columns if ap not in df.columns]
+    # 将缺失的 AP 添加到 df 中，并填充缺失值为 -100
+    print(f'{site_survey} miss {len(missing_aps)} aps')
+    print(missing_aps)
+    for ap in missing_aps:
+        df[ap] = -100
+        
+    # 确保列的顺序和 df0219 一致
+    df = df[df0219.columns]
+    # 使用插值處理缺失值
+    df = df.interpolate(method='linear', limit_direction='both')
+    # 如果還有缺失值，則填補為-100
+    df = df.fillna(-100)
+    
+    # 處理 label.csv
+    label_df = pd.read_csv(f'labels_{site_survey}.csv', header=0)
+    df['label'] = label_df['label']
+
+    mean_values = df.iloc[:, 1:].mean()
+    std_values = df.iloc[:, 1:].std()
+    
+    for ap in mean_values.index:
+        if std_values[ap] == 0:
+            df[ap] = 0  # 或者使用其他值代替 0
+        else:
+            df[ap] = (df[ap] - mean_values[ap]) / std_values[ap]
+            
+    # 將處理後的資料存為 csv 檔案
+    df.to_csv(f'data_{site_survey}.csv', index=False)
+
+
+# for df, name in zip([df0219, df1009, df0219], ['190219', '191009', '200219']):
+#     testing_set = pd.DataFrame(columns=df.columns)
+#     for label in df['label'].unique():
+#         sample = df[df['label'] == label].sample(n=1)
+#         testing_set = pd.concat([testing_set, sample], ignore_index=True)
+#         df = df.drop(sample.index)
+#         print(f"  Label{label} samples: {len(df[df['label'] == label])}")
+
+#     testing_set.to_csv(f'wireless_testing_{name}.csv', index=False)
+#     df.to_csv(f'wireless_training_{name}.csv', index=False)
 
